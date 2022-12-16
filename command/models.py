6d5d7501e450d9f012cd4d21bc4f7473daa6b9e7
TraitOtaku <traitotaku@gmail.com>
2022-12-17 05:58:54 +0900

    textfield

--- command/models.py
+++ command/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
+
 class Command(models.Model):
     title = models.CharField(max_length=300, blank=True, null=True)
     keyword = models.CharField(max_length=100, blank=True, null=True)
-    command = models.CharField(max_length=300, blank=True, null=True)
+    command = models.TextField(max_length=300, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Commands'
+        verbose_name_plural='Commands'
 
     def __str__(self):
         return self.title
 
 class Article(models.Model):
-    title = models.CharField(max_length=300, blank=True, null=True)
-    url = models.CharField(max_length=100, blank=True, null=True)
+    title=models.CharField(max_length=300, blank=True, null=True)
+    url=models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Articles'
+        verbose_name_plural='Articles'
 
     def __str__(self):
         return self.title
